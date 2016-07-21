ede919de1b8312f7ac2651c1f9558cbbe09ed2c3
speadup <speadup@gmail.com>
2016-07-21 23:28:36 +0800

    fix git.sh working path

--- git.sh
+++ git.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-[ -d .git ] || cd `dirname $0`/..
+#curdir=`pwd`
+basedir=`dirname $0`
+if [ -d .git ];then
+    unset basedir
+elif [ -d "$basedir/.git" ];then
+    cd $basedir
+elif [ -d "$basedir/../.git" ];then
+    cd $basedir/..
+else
+    exit 1
+fi
+unset basedir
 exec git "$@"
 
